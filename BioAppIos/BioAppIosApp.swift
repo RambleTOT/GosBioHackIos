5b8575736e365ccebc8c3a0ae47c755e05ede99e
RambleTOT <rambletot@gmail.com>
2024-10-13 11:14:10 +0500

    Initial Commit

---
+++ BioAppIos/BioAppIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioAppIosApp.swift
+//  BioAppIos
+//
+//  Created by Артём Сокерин on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BioAppIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
